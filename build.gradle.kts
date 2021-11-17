0bfa8e8733aad666027f6abd5c0f0069a3323f1e
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-11-16 21:17:49 -0800

    Update paperweight to 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "1.1.14"
+  id("io.papermc.paperweight.userdev") version "1.2.0"
   id("xyz.jpenilla.run-paper") version "1.0.4" // Adds runServer and runMojangMappedServer tasks for testing
   id("net.minecrell.plugin-yml.bukkit") version "0.5.0"
 }
