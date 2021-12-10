822967b6cbc0add8fbf11b2f3048853f8c2ab154
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-12-10 03:48:27 -0800

    Update run-paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 plugins {
   `java-library`
   id("io.papermc.paperweight.userdev") version "1.3.1"
-  id("xyz.jpenilla.run-paper") version "1.0.5" // Adds runServer and runMojangMappedServer tasks for testing
+  id("xyz.jpenilla.run-paper") version "1.0.6" // Adds runServer and runMojangMappedServer tasks for testing
   id("net.minecrell.plugin-yml.bukkit") version "0.5.1" // Generates plugin.yml
 }
 
