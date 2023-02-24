5b3922fde27ff25e47ade7382a77a065bbd9ae7e
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-02-24 13:14:46 -0700

    Update Gradle wrapper to 8.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   `java-library`
   id("io.papermc.paperweight.userdev") version "1.5.1"
   id("xyz.jpenilla.run-paper") version "2.0.1" // Adds runServer and runMojangMappedServer tasks for testing
-  id("net.minecrell.plugin-yml.bukkit") version "0.5.2" // Generates plugin.yml
+  id("net.minecrell.plugin-yml.bukkit") version "0.5.3" // Generates plugin.yml
 }
 
 group = "io.papermc.paperweight"
