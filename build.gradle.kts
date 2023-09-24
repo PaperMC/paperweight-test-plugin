d11af646c9a571cf9cbfd45dee402518fa4d7b1d
Arian100 <114690406+Arian100@users.noreply.github.com>
2023-09-24 11:18:57 +0200

    Update and add gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "1.5.5"
+  id("io.papermc.paperweight.userdev") version "1.5.6" // "1.5.7-SNAPSHOT"
   id("xyz.jpenilla.run-paper") version "2.2.0" // Adds runServer and runMojangMappedServer tasks for testing
 }
 
