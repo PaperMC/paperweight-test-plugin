3224ae1e4d63e28664607e898c1f4bb00ecebd13
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-03-27 12:58:51 -0700

    Update build dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "1.5.11"
+  id("io.papermc.paperweight.userdev") version "1.5.12"
   id("xyz.jpenilla.run-paper") version "2.2.3" // Adds runServer and runMojangMappedServer tasks for testing
 }
 
