c1d49ad8685430f3ea3643b752ce420d630bee9f
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-02 11:46:42 -0700

    Use `resource-factory-bukkit-convention` to generate plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
+
 plugins {
   `java-library`
   id("io.papermc.paperweight.userdev") version "1.5.12"
   id("xyz.jpenilla.run-paper") version "2.2.3" // Adds runServer and runMojangMappedServer tasks for testing
+  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.0.0" // Generates plugin.yml based on the Gradle config
 }
 
 group = "io.papermc.paperweight"
@@ -10,7 +13,7 @@ description = "Test plugin for paperweight-userdev"
 
 java {
   // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+  toolchain.languageVersion = JavaLanguageVersion.of(17)
 }
 
 dependencies {
@@ -30,30 +33,26 @@ tasks {
 
     // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
     // See https://openjdk.java.net/jeps/247 for more information.
-    options.release.set(17)
+    options.release = 17
   }
   javadoc {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-  }
-  processResources {
-    filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
-    val props = mapOf(
-      "name" to project.name,
-      "version" to project.version,
-      "description" to project.description,
-      "apiVersion" to "1.20"
-    )
-    inputs.properties(props)
-    filesMatching("plugin.yml") {
-      expand(props)
-    }
   }
 
   /*
   reobfJar {
     // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
     // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-    outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
+    outputJar = layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar")
   }
    */
+}
+
+// Configure plugin.yml generation
+// - name, version, and description are inherited from the Gradle project.
+bukkitPluginYaml {
+  main = "io.papermc.paperweight.testplugin.TestPlugin"
+  load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+  authors.add("Author")
+  apiVersion = "1.20"
 }
