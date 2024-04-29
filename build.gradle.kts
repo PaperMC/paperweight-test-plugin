73e125c2b04c00d65d7e4e45f8c6e7bdf7e17988
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-29 16:31:08 -0700

    1.20.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,11 @@ java {
 }
 
 // 1)
-// For 1.20.5+ when you don't care about supporting spigot
+// For >=1.20.5 when you don't care about supporting spigot
 // paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 // 2)
-// For 1.20.4 or below, or when you care about supporting Spigot on 1.20.5+
+// For 1.20.4 or below, or when you care about supporting Spigot on >=1.20.5
 // Configure reobfJar to run when invoking the build task
 /*
 tasks.assemble {
@@ -30,9 +30,9 @@ tasks.assemble {
  */
 
 dependencies {
-  paperweight.paperDevBundle("1.20.5-R0.1-SNAPSHOT")
-  // paperweight.foliaDevBundle("1.20.5-R0.1-SNAPSHOT")
-  // paperweight.devBundle("com.example.paperfork", "1.20.5-R0.1-SNAPSHOT")
+  paperweight.paperDevBundle("1.20.6-R0.1-SNAPSHOT")
+  // paperweight.foliaDevBundle("1.20.6-R0.1-SNAPSHOT")
+  // paperweight.devBundle("com.example.paperfork", "1.20.6-R0.1-SNAPSHOT")
 }
 
 tasks {
@@ -61,5 +61,5 @@ bukkitPluginYaml {
   main = "io.papermc.paperweight.testplugin.TestPlugin"
   load = BukkitPluginYaml.PluginLoadOrder.STARTUP
   authors.add("Author")
-  apiVersion = "1.20.5"
+  apiVersion = "1.20.6"
 }
