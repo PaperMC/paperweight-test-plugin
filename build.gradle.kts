6152a339d69d4445e6ee54a5b346aade10a7ee0c
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-01-15 13:13:20 -0700

    Update paperweight to 2.0.0-beta.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "2.0.0-beta.13"
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
   id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
   id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0" // Generates plugin.yml based on the Gradle config
 }
