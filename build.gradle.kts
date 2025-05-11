733c2165ad4dac75ff0de81cf101545f56dae1ff
Bjarne Koll <git@lynxplay.dev>
2025-05-11 17:28:02 +0200

    Upgrade to 1.21.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.16"
   id("xyz.jpenilla.run-paper") version "2.3.1" // Adds runServer and runMojangMappedServer tasks for testing
   id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.2.0" // Generates plugin.yml based on the Gradle config
 }
@@ -26,7 +26,7 @@ tasks.assemble {
  */
 
 dependencies {
-  paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
+  paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT")
   // paperweight.foliaDevBundle("1.21.4-R0.1-SNAPSHOT")
   // paperweight.devBundle("com.example.paperfork", "1.21.4-R0.1-SNAPSHOT")
 }
