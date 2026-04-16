332b75e347aeb01c088b5d05c25e2698bcb7e039
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2026-04-15 17:25:50 -0700

    Build with 26.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 
 plugins {
   `java-library`
-  id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
   id("xyz.jpenilla.run-paper") version "3.0.2" // Adds runServer and runMojangMappedServer tasks for testing
   id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.3.0" // Generates plugin.yml based on the Gradle config
 }
@@ -12,8 +12,8 @@ version = "1.0.0-SNAPSHOT"
 description = "Test plugin for paperweight-userdev"
 
 java {
-  // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
-  toolchain.languageVersion = JavaLanguageVersion.of(21)
+  // Configure the java toolchain. This allows gradle to auto-provision JDK 25 on systems that only have JDK 17 installed for example.
+  toolchain.languageVersion = JavaLanguageVersion.of(25)
 }
 
 // For 1.20.4 or below, or when you care about supporting Spigot on >=1.20.5:
@@ -26,16 +26,16 @@ tasks.assemble {
  */
 
 dependencies {
-  paperweight.paperDevBundle("1.21.10-R0.1-SNAPSHOT")
-  // paperweight.foliaDevBundle("1.21.10-R0.1-SNAPSHOT")
-  // paperweight.devBundle("com.example.paperfork", "1.21.10-R0.1-SNAPSHOT")
+  paperweight.paperDevBundle("26.1.2.build.+")
+  // paperweight.foliaDevBundle("26.1.2.build.+")
+  // paperweight.devBundle("com.example.paperfork", "26.1.2.build.+")
 }
 
 tasks {
   compileJava {
     // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
     // See https://openjdk.java.net/jeps/247 for more information.
-    options.release = 21
+    options.release = 25
   }
   javadoc {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
@@ -57,5 +57,5 @@ bukkitPluginYaml {
   main = "io.papermc.paperweight.testplugin.TestPlugin"
   load = BukkitPluginYaml.PluginLoadOrder.STARTUP
   authors.add("Author")
-  apiVersion = "1.21.10"
+  apiVersion = "26.1.2"
 }
